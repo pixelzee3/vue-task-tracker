55f51356090705346179208c1fa24175f3016787
pixelzee3 <bpixelzee@gmail.com>
2022-04-23 22:49:29 +0700

    Adjust styling.

--- src/TaskList.vue
+++ src/TaskList.vue
@@ -13,10 +13,14 @@ const props = defineProps({
     <div
       v-for="task in props.tasks"
       :key="task.id"
-      class="mt-4 rounded-lg px-2 py-4 first:mt-0"
-      :class="{ 'bg-green-100': task.completed, 'bg-gray-100': !task.completed }"
+      class="mt-4 flex justify-between rounded-lg px-2 py-4 first:mt-0"
+      :class="{
+        'bg-green-100': task.completed,
+        'bg-gray-100': !task.completed,
+      }"
     >
-      <h2 class="text-2xl font-semibold">{{ task.title }}</h2>
+      <h2 class="my-2 text-2xl font-semibold">{{ task.title }}</h2>
+      <button class="rounded bg-red-200 px-4">Delete</button>
     </div>
   </div>
 </template>
