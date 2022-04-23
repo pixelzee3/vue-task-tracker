d050540025474755022c4d997318d594563182e3
pixelzee3 <bpixelzee@gmail.com>
2022-04-23 22:38:51 +0700

    Style TaskList component.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,28 @@
 import { ref } from 'vue';
 import TaskList from './TaskList.vue';
 
-const tasks = ref([]);
+const tasks = ref([
+  {
+    id: 1,
+    title: 'Task 1',
+    completed: true,
+  },
+  {
+    id: 2,
+    title: 'Task 2',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Task 3',
+    completed: false,
+  },
+]);
 </script>
 <template>
   <div>
     <h1 class="mt-8 text-center text-4xl font-bold">Task Tracker</h1>
+    <div class="border-t-2 rounded mx-8 my-4"></div>
     <TaskList :tasks="tasks" />
   </div>
 </template>
--- src/TaskList.vue
+++ src/TaskList.vue
@@ -6,10 +6,17 @@ const props = defineProps({
     type: Array,
     default: () => [],
   },
-})
+});
 </script>
 <template>
-  <div>
-
+  <div class="px-8">
+    <div
+      v-for="task in props.tasks"
+      :key="task.id"
+      class="mt-4 rounded-lg px-2 py-4 first:mt-0"
+      :class="{ 'bg-green-100': task.completed, 'bg-gray-100': !task.completed }"
+    >
+      <h2 class="text-2xl font-semibold">{{ task.title }}</h2>
+    </div>
   </div>
 </template>
