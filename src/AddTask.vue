0d7504eb2a7aa498156cd04c8a7e8d5e1b2e0c98
pixelzee3 <bpixelzee@gmail.com>
2022-04-24 18:33:32 +0700

    Define styles for addtask component.

--- src/AddTask.vue
+++ src/AddTask.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+  import { ref } from 'vue';
+
+  const taskName = ref('');
+</script>
 <template>
-  <div></div>
+  <div class="mx-8">
+    <form class="flex justify-between space-x-4">
+      <input class="bg-slate-100 p-2 w-full grow" type="text" :v-model="taskName">
+      <button class="bg-blue-100 px-4 rounded ">Add</button>
+    </form>
+  </div>
 </template>
